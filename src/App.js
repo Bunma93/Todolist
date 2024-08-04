9c9e115b460842a7c52dc08b3bfd933cc4a02c09
Bunma93 <bunma93@gmail.com>
2024-08-04 15:31:09 +0700

    'finishheader'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
-
+import { FaHome } from 'react-icons/fa'
 function App() {
   return (
     <div className='container'>
-        <header className='header'>Header</header>
+        <header className='header'>
+          <span> 
+              <FaHome /> 
+          </span>
+            <h3>CC-todolist</h3>
+        </header>
 
         <aside className='sidebar'>Sidebar</aside>
 
