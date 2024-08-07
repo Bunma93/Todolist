6e6f4892cefa22f8bd20844e2091018caaf520ad
Bunma93 <bunma93@gmail.com>
2024-08-07 20:30:34 +0700

    finish:UI todo-list

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,4 +1,5 @@
 import styles from './Todocontent.module.scss'
+import {HiCheck, HiPencil, HiTrash} from 'react-icons/hi'
 
 export function TodoContent(){
     const mockTodo = Array.from({length:100},(el,idx) => idx+1);
@@ -14,15 +15,46 @@ export function TodoContent(){
 
     return (
     <main className='content'>
+
+    {/* Todoheader */}
     <div className={styles.header}>
-        <h2>Inbox</h2>
+        <h1>Inbox</h1>
         <p>{now.toLocaleDateString('en-US',options)}</p>
     </div>
-    <div>
-      Add todo
+
+    {/* Addtodo */}
+    <div className={styles.add_todo}>
+        <span>+</span>
+        <h3>Add task</h3>
     </div>
+    {/* To do form */}
+    <form className={styles.todo_form_container}>
+        <input className={styles.todo_form_input} placeholder='Task Name' />
+        <div className={styles.todo_form_footer}>
+            <p className={styles.todo_error}>Title is required</p>
+            <div className={styles.todo_form_buttons}>
+                <button>Cancle</button>
+                <button>Add task</button>
+            </div>
+        </div>
+    </form>
+
     <ul>
-      {mockTodo.map((el)=><li key={el}>{`item-${el}`}</li>)}
+      {mockTodo.map((el)=>(
+        <li className={styles.todo_items_container} key={el}>
+            <div className={styles.checkbox_container}>
+                <HiCheck className={styles.checkbox_icon} />
+            </div>
+            <p className={styles.done}>{`item-${el}`}</p>
+            <div className={styles.edit_icon}>
+                <HiPencil/>
+            </div>
+
+            <div className={styles.delete_icon}>
+                <HiTrash/>
+            </div>
+        </li>
+      ))}
     </ul>
   </main>
     );
