8beab686a0de8f11c97234f7d00ff02bbd163f74
Bunma93 <bunma93@gmail.com>
2024-08-08 05:27:59 +0700

    finish:AddTodoFuction

--- src/components/Todo/TodoContent.jsx
+++ src/components/Todo/TodoContent.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import { AddTodo } from './AddTodo'
 import { TodoHeader } from './TodoHeader'
@@ -7,11 +8,24 @@ import mockData from '../../data/todos.json';
 
 export function TodoContent(){
     const [todos, setTodos] = useState(mockData);
-    console.log(todos);
+
+    const handleAddTodo = () => {
+        //มี new todo
+        let newTodoObj = {id:uuidv4(), task:"DoHW" ,status: false, due_date: ""}
+
+        //สร้าง state ใหม่ ต่อจากเดิม
+        //ubdate  state by newState
+        // const newTodos = [newTodoObj,...todos]
+        // setTodos(newTodos);
+
+        //อีกวิธีในการอัพเดท state Callback
+        setTodos(currentState=> [newTodoObj,...currentState])
+    }
     return (
     <main className='content'>
         <TodoHeader/>
         <AddTodo/>
+        <button onClick={handleAddTodo}>testadd</button>
         <TodoList todos={todos}/>
     </main>
     );
